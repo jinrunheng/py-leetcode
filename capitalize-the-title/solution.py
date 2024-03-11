079acf1f8ff2fa112167e026a962330f8ba3b084
jinrunheng <1175088275@qq.com>
2024-03-11 22:40:19 +0800

    20240311

---
+++ capitalize-the-title/solution.py
@@ -0,0 +1,18 @@
+# 2129. 将标题首字母大写
+# https://leetcode.cn/problems/capitalize-the-title/description
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        split = title.split(' ')
+        res = []
+        for s in split:
+            if len(s) > 2:
+                res.append(s.title())
+            else:
+                res.append(s.capitalize())
+        return ' '.join(res)
+
+
+solution = Solution()
+title = "capiTalIze tHe titLe"
+res = solution.capitalizeTitle(title)
+print(res)
