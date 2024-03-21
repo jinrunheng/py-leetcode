df7a16cd811a17812695a8a393eaa27e4d29c604
jinrunheng <1175088275@qq.com>
2024-03-22 00:34:00 +0800

    finish 20240321

---
+++ frequency-tracker/solution.py
@@ -0,0 +1,20 @@
+class FrequencyTracker:
+
+    def __init__(self):
+        self.freq = [0] * 100001
+        self.freq_cnt = [0] * 100001
+
+    def add(self, number: int) -> None:
+        self.freq_cnt[self.freq[number]] -= 1
+        self.freq[number] += 1
+        self.freq_cnt[self.freq[number]] += 1
+
+    def deleteOne(self, number: int) -> None:
+        if self.freq[number] <= 0:
+            return
+        self.freq_cnt[self.freq[number]] -= 1
+        self.freq[number] -= 1
+        self.freq_cnt[self.freq[number]] += 1
+
+    def hasFrequency(self, frequency: int) -> bool:
+        return self.freq_cnt[frequency] > 0
