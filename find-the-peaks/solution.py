c0993e2b8c67af4c790c515faeb87ec74fe7c231
jinrunheng <1175088275@qq.com>
2024-05-28 23:01:06 +0800

    20240528

---
+++ find-the-peaks/solution.py
@@ -0,0 +1,12 @@
+# 2951. 找出峰值
+# https://leetcode.cn/problems/find-the-peaks/description/
+from typing import List
+
+
+class Solution:
+    def findPeaks(self, mountain: List[int]) -> List[int]:
+        res = []
+        for i in range(1, len(mountain) - 1):
+            if mountain[i - 1] < mountain[i] > mountain[i + 1]:
+                res.append(i)
+        return res
